1392c95f4a36efd34d87e1d76caaecf25855c0f4
dgr-crenshaw <dgrc@crowswood.com>
2016-03-24 09:17:27 -0500

    .ino reports errors Type Function 'snprintf' could not be resolved but compiles, uploads and runs

---
+++ cyberHiveWiFi/.ino.cpp
@@ -0,0 +1,17 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-03-24 09:01:48
+
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <DHT.h>
+void handle_root() ;
+void read_sensor() ;
+void setup(void) ;
+void loop(void) ;
+
+#include "cyberHiveWiFi.ino"
+
---
+++ cyberHiveWiFi/Release/.ino.cpp.d
@@ -0,0 +1,162 @@
+.ino.cpp.o: ../.ino.cpp \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Arduino.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/stdlib_noniso.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/binary.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/esp8266_peri.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/c_types.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/twi.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Arduino.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/core_esp8266_features.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/pgmspace.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/ets_sys.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/c_types.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/eagle_soc.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/osapi.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/user_config.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/WCharacter.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/WString.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/pgmspace.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/HardwareSerial.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Stream.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Print.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Printable.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/uart.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Esp.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Updater.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/flash_utils.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/MD5Builder.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/md5.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/debug.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/variants/d1_mini/pins_arduino.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFi.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/include/wl_definitions.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/IPAddress.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/WString.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Printable.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFiType.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/queue.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFiSTA.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFiGeneric.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFiAP.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFiScan.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/WiFiClient.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Print.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Client.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/IPAddress.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/include/slist.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/WiFiServer.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Server.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/WiFiClientSecure.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/include/ssl.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/WiFiClient.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WebServer/src/ESP8266WebServer.h \
+ /home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WebServer/src/detail/RequestHandler.h \
+ /home/dgrc/arduinoLibraries/DHT/DHT.h ../cyberHiveWiFi.ino
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Arduino.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/stdlib_noniso.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/binary.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/esp8266_peri.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/c_types.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/twi.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Arduino.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/core_esp8266_features.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/pgmspace.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/ets_sys.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/c_types.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/eagle_soc.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/osapi.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/user_config.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/WCharacter.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/WString.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/pgmspace.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/HardwareSerial.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Stream.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Print.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Printable.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/uart.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Esp.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Updater.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/flash_utils.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/MD5Builder.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/md5.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/debug.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/variants/d1_mini/pins_arduino.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFi.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/include/wl_definitions.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/IPAddress.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/WString.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Printable.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFiType.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/tools/sdk/include/queue.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFiSTA.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFiGeneric.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFiAP.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/ESP8266WiFiScan.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/WiFiClient.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Print.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Client.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/IPAddress.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/include/slist.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/WiFiServer.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/cores/esp8266/Server.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/WiFiClientSecure.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/include/ssl.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WiFi/src/WiFiClient.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WebServer/src/ESP8266WebServer.h:
+
+/home/dgrc/.arduino15/packages/esp8266/hardware/esp8266/2.1.0/libraries/ESP8266WebServer/src/detail/RequestHandler.h:
+
+/home/dgrc/arduinoLibraries/DHT/DHT.h:
+
+../cyberHiveWiFi.ino:
